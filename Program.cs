2676862f145b884392be30e8946f0f5dec7d8586
Ennaom <marta.kmiecik111@gmail.com>
2023-01-18 19:56:17 +0100

    create menu structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1. Add Task\n2. Edit Task\n3. Delete Task\n4. Exit Program");
+        Menu();
+    }
+
+    static void Menu()
+    {
+        start:
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                // TODO: add task
+                    break;
+
+                case 2:
+                // TODO: edit task
+                    break;
+
+                case 3:
+                // TODO: delete task
+                    break;
+                
+                case 4:
+                // TODO: exit program
+                    break;
+            }
+    }
+}
